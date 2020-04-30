d724c53760d91b33723b91cb52d5eb4ed49f7d52
Yiran Peng <44957440+yipen@users.noreply.github.com>
2020-04-30 14:47:23 +0800

    add gray code

--- src/BackTracking/LetterCombinationsofaPhoneNumber.java
+++ src/BackTracking/LetterCombinationsofaPhoneNumber.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class LetterCombinationsofaPhoneNumber {
     public List<String> letterCombinations(String digits) {
         LinkedList<String> ans = new LinkedList<String>();
+        if(digits.length()==0) return ans;
         String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         ans.add("");
         for (int i = 0; i < digits.length(); i++) {
